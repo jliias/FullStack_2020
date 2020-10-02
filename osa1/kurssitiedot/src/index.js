99904672915f48523fff408eecde973d11125101
Juha Liias <juha.liias@gmail.com>
2020-10-02 23:31:48 +0300

    step1, step2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Part = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  );
+}
+
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        <Part part="part1" exercises="5" />
+        {/* <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} /> */}
+      </p>
+    </>
+  );
+}
+
+const Total = (props) => {
+  return (
+    <>
+      {props.total}
+    </>
+  );
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +50,10 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} />
+
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
