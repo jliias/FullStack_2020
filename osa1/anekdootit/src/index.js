94e3a9f9620416bca8c57c224968305d2bcab54a
Juha Liias <juha.liias@gmail.com>
2020-10-07 00:29:04 +0300

    part 1.13 done

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -1,27 +1,67 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+function getArrayMax(array){
+  return Math.max.apply(null, array);
+}
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
 )
 
+const MostVoted = (allVotes) => {
+  console.log(allVotes)
+  // const mostVoted = Math.max.apply(null, allVotes)
+  // let largest = 0;
+
+  // for (var i = 0; i <= allVotes.length; i++) {
+  //   if (allVotes[i] > largest) {
+  //     console.log("i: ", i)
+  //     largest = allVotes[i];
+  //   }
+  // }
+
+  const largest = getArrayMax(allVotes)
+  console.log("largest: ", largest)
+  return (
+    <div>
+      ahaa
+      {/* {anecdotes[Math.max(...allVotes)]} */}
+    </div>
+  );
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-
+  //const allVotes = new Array(anecdotes.length).fill(0)
+  const [allVotes, setAll] = useState(Array(anecdotes.length).fill(0))
   const nextAnecdote = () => setSelected(Math.floor(Math.random() * Math.floor(anecdotes.length)))
+
+  const makeVote = () => {
+    allVotes[selected] += 1
+    console.log(allVotes);
+  }
 
   console.log(selected);
 
+
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {props.anecdotes[selected]}
-      <br/>
+      <br />
+      <Button
+        text="vote"
+        handleClick={makeVote}
+      />
       <Button
         text="next anecdote"
         handleClick={nextAnecdote}
       />
+      <h1>Anecdote with most votes</h1>
+      <MostVoted allVotes={allVotes} />
     </div>
   )
 }
