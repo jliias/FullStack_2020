6973da9e9df9596098ce21be4e380af3f55bba12
Juha Liias <juha.liias@gmail.com>
2020-10-15 00:53:17 +0300

    osa2 all tasks done

--- osa2/kurssitiedot/src/Components/Course.js
+++ osa2/kurssitiedot/src/Components/Course.js
@@ -28,19 +28,24 @@ const Content = ({ course }) => {
     return (
         <div>
             {course.parts.map(part =>
-                <Part part={part} 
-                key={part.id}/>
+                <Part part={part}
+                    key={part.id} />
             )}
         </div>
     )
 }
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
+    console.log(courses)
     return (
         <div>
-            <Header course={course} />
-            <Content course={course} />
-            <Total course={course} />
+            {courses.map(course =>
+                <div key={course.id}> 
+                    <Header course={course} />
+                    <Content course={course} />
+                    <Total course={course} />
+                </div>
+            )}
         </div>
     )
 }
