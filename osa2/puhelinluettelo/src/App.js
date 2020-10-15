3a5f5c5107655ce5c6af346ef0458d6ebb9cf46f
Juha Liias <juha.liias@gmail.com>
2020-10-15 23:42:13 +0300

    puhelinluettelo step4

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -7,10 +7,14 @@ const App = () => {
             name: 'Arto Hellas',
             number: '040-1231244'
             // id: 1
-        }
+        },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
     ])
     const [newName, setNewName] = useState('add new person')
     const [newNumber, setNewNumber] = useState('add number')
+    const [myString, setNewString] = useState('')
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -24,6 +28,7 @@ const App = () => {
                 found = true
             }
         })
+
         if (found) {
             console.log("FOUND!")
             alert(`${newName} already added to phonebook`)
@@ -49,9 +54,24 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const handleStringChange = (event) => {
+        // console.log(event.target.value)
+        setNewString(event.target.value)
+    }
+
+    const personsToShow = myString.length > 0 
+    ? persons.filter(person => person.name.toUpperCase().includes(myString.toUpperCase()))
+    : persons
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>
+                Filter shown with <input value={myString}
+                    onChange={handleStringChange}
+                />
+            </div>
+            <h2>add a new</h2>
             <form onSubmit={addPerson}>
                 <div>
                     name: <input value={newName}
@@ -68,8 +88,8 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {persons.map(person =>
-                <Person key={person.name} person={person}/>
+            {personsToShow.map(person =>
+                <Person key={person.name} person={person} />
             )}
         </div>
     )
