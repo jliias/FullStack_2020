cdd5493c49e736b9b7eb9d7ca81cbda8ad4d6746
Juha Liias <juha.liias@gmail.com>
2020-10-15 22:44:51 +0300

    puhelinluettelo step2

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -8,17 +8,31 @@ const App = () => {
             // id: 1
         }
     ])
-    const [newName, setNewName] = useState('')
+    const [newName, setNewName] = useState('add new person')
 
     const addPerson = (event) => {
         event.preventDefault()
-        const nameObject = {
-            name: newName
-            //id: persons.length + 1,
-        }
 
-        setPersons(persons.concat(nameObject))
-        setNewName('')
+        // Did not get array.include() working, so implemented
+        // with forEach loop
+        let found = false
+        persons.forEach(function (item, index, array) {
+            console.log("item-index ", item, index)
+            if (item.name === newName) {
+                found = true
+            }
+        })
+        if (found) {
+            console.log("FOUND!")
+            alert(`${newName} already added to phonebook`)
+        } else {
+            const nameObject = {
+                name: newName
+                //id: persons.length + 1,
+            }
+            setPersons(persons.concat(nameObject))
+            setNewName('')
+        }
     }
 
     const handleNameChange = (event) => {
@@ -38,11 +52,9 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            <ul>
-                {persons.map(person =>
-                    <Person key={person.name} person={person} />
-                )}
-            </ul>
+            {persons.map(person =>
+                <Person key={person.name} person={person} />
+            )}
         </div>
     )
 
