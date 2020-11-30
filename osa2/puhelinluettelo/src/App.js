02d48a5f5a9e1c96b33c745605436eea2e7034a6
Juha Liias <juha.liias@gmail.com>
2020-12-01 00:16:25 +0200

    puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './Components/Filter'
 import PersonForm from './Components/PersonForm'
 import Persons from './Components/Persons'
 
+import axios from 'axios';
+
 const App = () => {
-    const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '040-1231244'
-            // id: 1
-        },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ])
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('add new person')
     const [newNumber, setNewNumber] = useState('add number')
     const [myString, setNewString] = useState('')
+
+    // Use Effect hook to fetch initial data from JSON server
+    useEffect(() => {
+        console.log('effect')
+        axios
+          .get('http://localhost:3001/persons')
+          .then(response => {
+            console.log('promise fulfilled')
+            setPersons(response.data)
+          })
+      }, [])
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -82,7 +86,6 @@ const App = () => {
             <Persons personsToShow={personsToShow} />
         </div>
     )
-
 }
 
 export default App
